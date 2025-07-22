5b21b32608cee2bc9e1fa05bba3c0c7ad5bb68d0
karavachev-k-kiril <karavachev.kiril.hosting@gmail.com>
2025-07-22 11:00:34 +0300

    Created a reducer to consolidate all of the state update logic.

---
+++ ic-technical-task-step-2/src/reducer.jsx
@@ -0,0 +1,27 @@
+export const initialState = {
+  country: 'PL',
+  perPage: '10',
+  availability: '-',
+  brand: '-',
+  searchQuery: '',
+};
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_COUNTRY':
+      return { ...state, country: action.payload, page: 1 };
+    case 'SET_PER_PAGE':
+      return { ...state, perPage: action.payload, page: 1 };
+    case 'SET_AVAILABILITY':
+      return { ...state, availability: action.payload, page: 1 };
+    case 'SET_BRAND':
+      return { ...state, brand: action.payload, page: 1 };
+    case 'SET_SEARCH_QUERY':
+      return { ...state, searchQuery: action.payload };
+    case 'SEARCH':
+
+      return { ...state, page: 1 };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
