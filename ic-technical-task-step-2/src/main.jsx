eb64cae846f98d64d857ebc1776924c2c3c51318
karavachev-k-kiril <karavachev.kiril.hosting@gmail.com>
2025-07-22 11:30:38 +0300

    Added StateProvider and ErrorBoundary to main.jsx

--- ic-technical-task-step-2/src/main.jsx
+++ ic-technical-task-step-2/src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StateProvider } from './context/StateContext';
+import ErrorBoundary from './components/ErrorBoundary'; // Import ErrorBoundary
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <StateProvider>
+        <App />
+      </StateProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
