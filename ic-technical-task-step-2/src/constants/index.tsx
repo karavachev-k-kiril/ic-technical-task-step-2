919b75b56e0fae6f7996d98790d42cc059dcf642
karavachev-k-kiril <karavachev.kiril.hosting@gmail.com>
2025-07-23 11:37:43 +0300

    Changed jsx to tsx.

---
+++ ic-technical-task-step-2/src/constants/index.tsx
@@ -0,0 +1,25 @@
+export const API_URL = 'https://motointegrator.com/api/search/v4/workshop';
+
+export const COUNTRIES = [
+  { name: 'Poland', code: 'PL' },
+  { name: 'Bulgaria', code: 'BG' },
+  { name: 'Hungary', code: 'HU' },
+  { name: 'Czech Republic', code: 'CZ' },
+  { name: 'Bosnia And Herzegovina', code: 'BA' },
+  { name: 'Lithuania', code: 'LT' },
+  { name: 'Slovenia', code: 'SI' },
+  { name: 'Latvia', code: 'LV' },
+  { name: 'Estonia', code: 'EE' },
+  { name: 'Ukraine', code: 'UA' },
+  { name: 'Croatia', code: 'HR' },
+  { name: 'Romania', code: 'RO' },
+  { name: 'Slovakia', code: 'SK' },
+];
+
+export const OPEN_HOURS_OPTIONS = [
+  { name: 'Any', value: '-' },
+  { name: 'Open now', value: 'OPEN_NOW' },
+  { name: 'Open 24H', value: 'OPEN_24H' },
+  { name: 'Open after 18:00', value: 'OPEN_AFTER_18' },
+  { name: 'Open on weekends', value: 'OPEN_ON_WEEKENDS' },
+];
