089c4b7e9b925287e88f536b71ab6c6697e534ae
karavachev-k-kiril <karavachev.kiril.hosting@gmail.com>
2025-07-23 12:09:56 +0300

    Reworked to use Type Script.

---
+++ ic-technical-task-step-2/src/components/FavoritesListItem/FavoritesListItem.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import HeartIcon from '../UI/Icons/HeartIcon';
+import PhoneIcon from '../UI/Icons/PhoneIcon';
+import './FavoritesListItem.css';
+import { Workshop } from '../../constants/types';
+
+interface FavoritesListItemProps {
+    ws: Workshop;
+    onToggleFav: (ws: Workshop) => void;
+    countryCode: string;
+}
+
+const FavoritesListItem: React.FC<FavoritesListItemProps> = ({ ws, onToggleFav, countryCode }) => {
+    const linkUrl = `https://motointegrator.com/${countryCode.toLowerCase()}/en/carworkshop/${ws.address?.localitySlug}/${ws.hashedKhCode}-${ws.slug}`;
+
+    return (
+        <div className="favorite-list-item">
+            <a
+                href={linkUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                title={`View details for ${ws.name}`}
+                className="list-item-link"
+            >
+                <div className="list-item-content">
+                    <span className="list-item-name">{ws.name}</span>
+                    {ws.address?.locality && (
+                        <><br/><span className="list-item-location">{ws.address.locality}</span></>
+                    )}
+                </div>
+            </a>
+
+            <div className="list-item-actions">
+                {ws.phoneNumber && (
+                    <a href={`tel:${ws.phoneNumber}`} className="list-item-action-phone" title={`Call ${ws.name}`}>
+                        <PhoneIcon />
+                    </a>
+                )}
+
+                <button onClick={() => onToggleFav(ws)} title="Toggle Favorite" className="favorite-list-toggle-button">
+                    <HeartIcon isFav={true} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default React.memo(FavoritesListItem);
